d0c8150496acc8631a635e93332671bc9adb4dd0
aldygans <kemedrifaldy@gmail.com>
2025-01-29 20:39:24 +0700

    navbar

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
-    document.getElementById('mobile-menu').classList.toggle('hidden');
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden', 'hide');
+        mobileMenu.classList.add('show');
+    } else {
+        mobileMenu.classList.remove('show');
+        mobileMenu.classList.add('hide');
+        setTimeout(() => {
+            mobileMenu.classList.add('hidden');
+        }, 300); // Waktu yang sama dengan durasi transisi
+    }
 });
+
+    
+document.getElementById('menu-toggle').addEventListener('click', function() {
+        const mobileMenu = document.getElementById('mobile-kontak');
+        mobileMenu.classList.toggle('-translate-y-full');
+        mobileMenu.classList.toggle('hidden');
+    });
